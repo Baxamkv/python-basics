b962f04768a24d39dae2f7d503da99b9d082102b
Baxamkv <akhmetbatyrkhan@gmail.com>
2026-01-31 13:12:19 +0400

    ADD PP2

---
+++ datatype.py
@@ -0,0 +1,21 @@
+"""Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType"""
+
+x = 1j
+y = 3.14
+z = 39
+
+#convertings
+
+a = int(y)
+b = float(z)
+c = complex(y)
+
+print(a); print(b); print(c)
+
